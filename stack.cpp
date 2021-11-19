ec946b324778473d60573b3d7c066440f03ddc3a
Pennyblack2008 <g2ang12@gmail.com>
2021-11-19 19:44:07 +0900

    Feat: Stack Test 통과

---
+++ stack.cpp
@@ -0,0 +1,26 @@
+#include "stack/stack.hpp"
+#include <iostream>
+#include <stack>
+#include <string>
+#include "include/iterator.hpp"
+#include <deque>          // std::deque
+#include <vector>
+
+int main ()
+{
+  std::deque<int> mydeque (3,100);          // deque with 3 elements
+  std::vector<int> myvector (2,200);        // vector with 2 elements
+
+  ft::stack<int> first;                    // empty stack
+  ft::stack<int> second (mydeque);         // stack initialized to copy of deque
+
+  ft::stack<int,std::vector<int> > third;  // empty stack using vector
+  ft::stack<int,std::vector<int> > fourth (myvector);
+
+  std::cout << "size of first: " << first.size() << '\n';
+  std::cout << "size of second: " << second.size() << '\n';
+  std::cout << "size of third: " << third.size() << '\n';
+  std::cout << "size of fourth: " << fourth.size() << '\n';
+
+  return 0;
+}
--- stack/stack.hpp
+++ stack/stack.hpp
@@ -1,19 +1,31 @@
 #ifndef _STACK_HPP_
 #define _STACK_HPP_
-#include "vector.hpp"
+#include "../vector/vector.hpp"
 
 namespace ft
 {
     template <class T, class Container = vector<T> >
     class stack
     {
+        typedef typename Container::value_type      Container_value_type;
+
+        template<typename T1, typename Container1>
+        friend bool
+            operator==(const stack<T1, Container1>&, const stack<T1, Container1>&);
+
+        template<typename T1, typename Container1>
+        friend bool
+            operator<(const stack<T1, Container1>&, const stack<T1, Container1>&);
+
     public:
-        typedef typename Container::value_type value_type;
-        typedef typename Container::size_type size_type;
-        typedef Container container_type;
+        typedef typename Container::value_type      value_type;
+        typedef typename Container::reference       reference;
+        typedef typename Container::const_reference const_reference;
+        typedef typename Container::size_type       size_type;
+        typedef          Container                  container_type;
 
     protected:
-        container_type c;
+        Container c;
     
     public:
         explicit stack(const container_type& c = container_type())
@@ -21,20 +33,69 @@ namespace ft
 
         bool empty() const { return c.empty(); }
         size_type size() const { return c.size(); }
-        value_type& top() { return c.back(); }
-        const value_type& top() const { return c.back(); }
+        reference top() { return c.back(); }
+        const_reference top() const { return c.back(); }
         void push(const value_type& x) { c.push_back(x); }
         void pop() { c.pop_back(); }
-    
-    private:
-        template <typename T, typename Container>
-        friend bool operator==(const stack<T, Container>&, const stack<T, Container>&);
-
-        template <typename T, typename Container>
-        friend bool operator<(const stack<T, Container>&, const stack<T, Container>&);
     };
+    /**
+     *  @brief  Stack equality comparison.
+     *  @param  x  A %stack.
+     *  @param  y  A %stack of the same type as @a x.
+     *  @return  True iff the size and elements of the stacks are equal.
+     *
+     *  This is an equivalence relation.  Complexity and semantics
+     *  depend on the underlying sequence type, but the expected rules
+     *  are: this relation is linear in the size of the sequences, and
+     *  stacks are considered equivalent if their sequences compare
+     *  equal.
+     */
+    template<typename T, typename Container>
+    inline bool
+        operator==(const stack<T, Container>& x, const stack<T, Container>& y)
+    { return x.c == y.c; }
 
-    template <class T, 
+    /**
+     *  @brief  Stack ordering relation.
+     *  @param  x  A %stack.
+     *  @param  y  A %stack of the same type as @a x.
+     *  @return  True iff @a x is lexicographically less than @a y.
+     *
+     *  This is an total ordering relation.  Complexity and semantics
+     *  depend on the underlying sequence type, but the expected rules
+     *  are: this relation is linear in the size of the sequences, the
+     *  elements must be comparable with @c <, and
+     *  std::lexicographical_compare() is usually used to make the
+     *  determination.
+     */
+    template<typename T, typename Container>
+        inline bool
+    operator<(const stack<T, Container>& x, const stack<T, Container>& y)
+    { return x.c < y.c; }
+
+    /// Based on operator==
+    template<typename T, typename Container>
+        inline bool
+    operator!=(const stack<T, Container>& x, const stack<T, Container>& y)
+    { return !(x == y); }
+
+    /// Based on operator<
+    template<typename T, typename Container>
+        inline bool
+    operator>(const stack<T, Container>& x, const stack<T, Container>& y)
+    { return y < x; }
+
+     /// Based on operator<
+    template<typename T, typename Container>
+    inline bool
+        operator<=(const stack<T, Container>& x, const stack<T, Container>& y)
+    { return !(y < x); }
+
+    /// Based on operator<
+    template<typename T, typename Container>
+    inline bool
+        operator>=(const stack<T, Container>& x, const stack<T, Container>& y)
+    { return !(x < y); }
 }
 
 #endif
